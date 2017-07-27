f12eed8e6ca0288f6669ac2b7581c17fad998763
Brian Reichholf <brian.reichholf@gmail.com>
2017-07-27 16:54:08 +0200

    Adding summarise_pos.R

---
+++ bin/summarise_pos.R
@@ -0,0 +1,44 @@
+require(dplyr)
+require(purrr)
+require(Rsamtools)
+
+calc.maxpos <- function(id, time, align, pos) {
+  require(stringr)
+  mapInfo <- c("rname", "strand", "pos")
+  mapParams <- ScanBamParam(what = mapInfo, tag = c("TC", "TN"))
+  bam <- scanBam(file.path(file.home, align), param = mapParams)
+  # Now this will ONLY handle files that have tags TC and TN, too!
+  map.r <- dplyr::bind_cols(do.call(bind_cols, bam[[1]][mapInfo]), do.call(bind_cols, bam[[1]]$tag))
+  # map.r <- dplyr::bind_cols(bam)
+  pos.only <-
+    map.r %>%
+    dplyr::filter(strand == "+")
+
+  r.summary <-
+    pos.only %>%
+    group_by(rname, pos) %>%
+    summarise(count := n()) %>% ungroup() %>%
+    mutate(flybase_id := as.character(rname))
+
+  r.sum.pos <-
+    r.summary %>%
+    left_join(mir.anno, by="flybase_id") %>%
+    select(-rname, -loop)
+
+  r.sum.arms <-
+    r.sum.pos %>%
+    dplyr::filter((pos >= `5p` - 5 & pos <= `5p` + 5) | (pos >= `3p` - 5 & pos <= `3p` + 5)) %>%
+    mutate(arm.name = ifelse(pos >= `5p` - 5 & pos <= `5p` + 5,
+                             paste0(str_sub(mir_name, 5, -1), "-5p"),
+                             paste0(str_sub(mir_name, 5, -1), "-3p")))
+
+  countName <- paste("count", id, sep = ".")
+
+  r.sum.max.pos <-
+    r.sum.arms %>%
+    group_by(arm.name) %>%
+    top_n(n = 5, wt = count) %>% ungroup() %>%
+    rename_(.dots = setNames("count", countName))
+
+  return(r.sum.max.pos)
+}
